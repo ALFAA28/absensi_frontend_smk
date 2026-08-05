f1879c3df7272d45a37a8c6e0d941d92d4371ed8
ALFAA28 <162124253+ALFAA28@users.noreply.github.com>
2026-08-05 09:34:12 +0700

    a

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // URL API akan mengambil dari file .env di environment (REACT_APP_API_URL)
 // Jika tidak ada, maka secara default akan mengarah ke http://localhost:8000/api
 
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://absensi-backend-smk.onrender.com';
