0f048a491e05d76fa6bf77f69368a046fb2dcfb0
ALFAA28 <162124253+ALFAA28@users.noreply.github.com>
2026-09-25 16:57:32 +0700

    Update otomatis dari script push_semua

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -58,7 +58,7 @@ const Sidebar = ({ isOpen, toggleSidebar, closeSidebar }) => {
         )}
 
         {/* Link eksternal ke Web Storing Modul (Tidak untuk Admin Absensi) */}
-        {(role === 'wali_kelas' || role === 'guru_mapel' || role === 'guru_piket' || role === 'guru') && (
+        {(role === 'wali_kelas' || role === 'guru_mapel' || role === 'guru') && (
           <a href="https://storing-modul-main.vercel.app" target="_blank" rel="noopener noreferrer" className="nav-item">
             <FiFileText className="nav-icon" />
             <span>Arsip Modul (Web Baru)</span>
